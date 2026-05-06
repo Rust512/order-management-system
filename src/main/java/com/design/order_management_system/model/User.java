ffacb1f35e632e004f91811ad442a7c3bf2cd379
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-06 22:18:20 +0530

    Add constraints to the User entity

--- src/main/java/com/design/order_management_system/model/User.java
+++ src/main/java/com/design/order_management_system/model/User.java
@@ -1,11 +1,13 @@
 package com.design.order_management_system.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,12 +16,18 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-@Table(name = "_users")
+@Table(name = "app_users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
     private String username;
+
+    @NotBlank
+    @Column(nullable = false)
     private String password;
     
     @OneToMany(mappedBy = "user")
