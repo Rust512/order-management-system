04f8119a5f29b563618db84bf758b47af914d986
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-06 22:10:33 +0530

    Remove unused imports

--- src/main/java/com/design/order_management_system/model/User.java
+++ src/main/java/com/design/order_management_system/model/User.java
@@ -1,20 +1,14 @@
 package com.design.order_management_system.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
 import java.util.Set;
 
 @Entity
