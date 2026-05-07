79102a699ebeb3a67709e88ec70622134cd454b5
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 07:58:59 +0530

    Add user request and response DTOs

---
+++ src/main/java/com/design/order_management_system/dto/UserResponse.java
@@ -0,0 +1,19 @@
+package com.design.order_management_system.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserResponse {
+    @JsonProperty(value = "sUsername")
+    private String username;
+    
+    @JsonProperty(value = "aRoles")
+    private List<String> roles;
+}
