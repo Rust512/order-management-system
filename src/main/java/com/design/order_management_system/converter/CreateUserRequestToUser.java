258d1d0a7a25d0efa7f9af10980991b5224f24eb
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 17:15:05 +0530

    Use java.util.function.Function for converters

--- src/main/java/com/design/order_management_system/converter/CreateUserRequestToUser.java
+++ src/main/java/com/design/order_management_system/converter/CreateUserRequestToUser.java
@@ -2,15 +2,15 @@ package com.design.order_management_system.converter;
 
 import com.design.order_management_system.dto.CreateUserRequest;
 import com.design.order_management_system.model.User;
-import org.jspecify.annotations.NonNull;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Function;
+
 @Component
-public class CreateUserRequestToUser implements Converter<CreateUserRequest, User> {
+public class CreateUserRequestToUser implements Function<CreateUserRequest, User> {
 
     @Override
-    public User convert(@NonNull CreateUserRequest source) {
+    public User apply(CreateUserRequest source) {
         // The password should be encrypted, the logic will be added later.
         return User.builder()
                 .username(source.getUsername())
