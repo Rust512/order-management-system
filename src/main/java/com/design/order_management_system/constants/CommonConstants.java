d91111a9b5fa407c67d9529572c658898c191a5f
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 17:14:19 +0530

    Add a constants file to store string constants

---
+++ src/main/java/com/design/order_management_system/constants/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.design.order_management_system.constants;
+
+public class CommonConstants {
+    private CommonConstants() {
+    }
+    
+    public static final String ROLE_USER = "ROLE_USER";
+}
