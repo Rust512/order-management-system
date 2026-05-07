4ce8dac36455aaf27ca87e366e6a277db51c5704
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 07:30:14 +0530

    Optimize the Role entity
    The code changes in this commit:
    1. Add the unique constraint to the name property
    2. Mark the name field as non-nullable
    3. Mark the name field as not blank through validation
    4. Add fetch type to the users mapping
    5. Add cascading to the users mapping
    
    in the role entity.

--- src/main/java/com/design/order_management_system/model/Role.java
+++ src/main/java/com/design/order_management_system/model/Role.java
@@ -1,15 +1,18 @@
 package com.design.order_management_system.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
@@ -17,8 +20,16 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
     private String name;
-    
-    @OneToMany(mappedBy = "role")
-    List<UserRoleMapping> users;
+
+    @OneToMany(
+            mappedBy = "role",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    Set<UserRoleMapping> users = new HashSet<>();
 }
