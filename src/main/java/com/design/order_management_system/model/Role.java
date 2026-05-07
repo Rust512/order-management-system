3709305275ff42c49edee53138fddf171f000f80
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 07:38:51 +0530

    Add getters and setters to the Role entity

--- src/main/java/com/design/order_management_system/model/Role.java
+++ src/main/java/com/design/order_management_system/model/Role.java
@@ -10,10 +10,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
 public class Role {
