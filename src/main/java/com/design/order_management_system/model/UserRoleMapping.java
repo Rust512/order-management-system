e03ff7a3a6d6e494d9b4fd4b40a082afc2834459
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 16:48:12 +0530

    Add the @Builder annotation to UserRoleMapping

--- src/main/java/com/design/order_management_system/model/UserRoleMapping.java
+++ src/main/java/com/design/order_management_system/model/UserRoleMapping.java
@@ -7,12 +7,18 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user_role_mappings")
 public class UserRoleMapping {
     @EmbeddedId
