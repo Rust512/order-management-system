bba4323f364f51914bfd4efbb8a5c5b96044325d
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 17:23:07 +0530

    Add a user controller
    The added user controller has two methods:
    1. createUser (POST '/v1/user')
    2. getUser (GET '/v1/user/{id}')

---
+++ src/main/java/com/design/order_management_system/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.design.order_management_system.controller;
+
+import com.design.order_management_system.UserService;
+import com.design.order_management_system.dto.CreateUserRequest;
+import com.design.order_management_system.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    UserResponse createUser(@RequestBody CreateUserRequest createUserRequest) {
+        return userService.createUser(createUserRequest);
+    }
+    
+    @GetMapping(path = "/{id}")
+    UserResponse getUser(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+}
