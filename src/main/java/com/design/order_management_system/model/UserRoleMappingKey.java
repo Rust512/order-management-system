cee88881094549b3d84a756cf35f4f79d7e6abf2
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-07 17:16:55 +0530

    Add @Builder annotation to the UserRoleMappinKey

--- src/main/java/com/design/order_management_system/model/UserRoleMappingKey.java
+++ src/main/java/com/design/order_management_system/model/UserRoleMappingKey.java
@@ -2,16 +2,22 @@ package com.design.order_management_system.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@Builder
 @Embeddable
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRoleMappingKey implements Serializable {
     @Column(name = "user_id")
     private Long userId;
