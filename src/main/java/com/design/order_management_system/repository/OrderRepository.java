d6885ebf677c750149cd5761f146fff1125e9e1e
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 20:23:17 +0530

    Add a find by ID method with EntityGraph

--- src/main/java/com/design/order_management_system/repository/OrderRepository.java
+++ src/main/java/com/design/order_management_system/repository/OrderRepository.java
@@ -1,7 +1,22 @@
 package com.design.order_management_system.repository;
 
 import com.design.order_management_system.model.domain.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query(value = """
+            SELECT o
+            FROM Order o
+            WHERE o.id = :id
+            """)
+    @EntityGraph(attributePaths = {
+            "orderItems.product.name",
+            "orderItems.quantity",
+            "orderItems.purchasePrice"
+    })
+    Optional<Order> getOrderByIdWithItems(Long id);
 }
