e908456ae6090a2b7cd1fe3d848f26b9170f7779
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 21:20:16 +0530

    Add a controller to fetch Products in pages

--- src/main/java/com/design/order_management_system/controller/ProductController.java
+++ src/main/java/com/design/order_management_system/controller/ProductController.java
@@ -1,15 +1,18 @@
 package com.design.order_management_system.controller;
 
 import com.design.order_management_system.dto.request.CreateProductRequest;
+import com.design.order_management_system.dto.response.PagedResponse;
 import com.design.order_management_system.dto.response.ProductResponse;
 import com.design.order_management_system.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +25,10 @@ public class ProductController {
     ResponseEntity<ProductResponse> registerProduct(@RequestBody @Valid CreateProductRequest createProductRequest) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(productService.registerProduct(createProductRequest));
+    }
+
+    @GetMapping
+    ResponseEntity<PagedResponse<ProductResponse>> getProducts(@RequestParam int page, @RequestParam int size) {
+        return ResponseEntity.ok(productService.getProducts(page, size));
     }
 }
