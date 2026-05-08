eb3c6dde82150320013b1e3b60682d228f6b19bb
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 11:59:18 +0530

    Add a controller to register products

---
+++ src/main/java/com/design/order_management_system/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.design.order_management_system.controller;
+
+import com.design.order_management_system.dto.CreateProductRequest;
+import com.design.order_management_system.dto.ProductResponse;
+import com.design.order_management_system.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    ResponseEntity<ProductResponse> registerProduct(@RequestBody @Valid CreateProductRequest createProductRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(productService.registerProduct(createProductRequest));
+    }
+}
