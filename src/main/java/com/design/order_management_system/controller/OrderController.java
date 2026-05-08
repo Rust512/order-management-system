8555722be90cfcbd329faa5000151702bca3ce29
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 16:20:48 +0530

    Add a controller to register orders

---
+++ src/main/java/com/design/order_management_system/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.design.order_management_system.controller;
+
+import com.design.order_management_system.dto.request.OrderRequest;
+import com.design.order_management_system.dto.response.OrderResponse;
+import com.design.order_management_system.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/v1/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    ResponseEntity<OrderResponse> registerOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        return ResponseEntity.ok(orderService.registerOrder(orderRequest));
+    }
+}
