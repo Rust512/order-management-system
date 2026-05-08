142395e9950ce385a122458e9b8916e85092adfa
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 11:56:57 +0530

    Add request and response DTOs for Product entity

--- src/main/java/com/design/order_management_system/dto/CreateProductRequest.java
+++ src/main/java/com/design/order_management_system/dto/CreateProductRequest.java
@@ -1,9 +1,10 @@
 package com.design.order_management_system.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +16,8 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductDTO {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreateProductRequest {
 
     @NotBlank
     @JsonProperty(value = "sProductName")
@@ -25,7 +27,7 @@ public class ProductDTO {
     @JsonProperty(value = "dPrice")
     private BigDecimal price;
 
-    @Min(value = 0L)
+    @PositiveOrZero
     @JsonProperty(value = "dStock")
     private Long stock;
 }
