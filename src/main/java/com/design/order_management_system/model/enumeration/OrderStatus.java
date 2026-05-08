ff1d34eaf354ae06939564336ee3951141dcb407
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 10:41:39 +0530

    Add the Order and OrderItem entities

---
+++ src/main/java/com/design/order_management_system/model/enumeration/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.design.order_management_system.model.enumeration;
+
+public enum OrderStatus {
+    CREATED,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
