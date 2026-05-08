1e9195e59c76066110bc323d6ba5f8ab4f7d2025
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 11:55:13 +0530

    Add the @NotBlank validation to UserResponse

--- src/main/java/com/design/order_management_system/dto/UserResponse.java
+++ src/main/java/com/design/order_management_system/dto/UserResponse.java
@@ -1,6 +1,7 @@
 package com.design.order_management_system.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,8 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 public class UserResponse {
+
+    @NotBlank
     @JsonProperty(value = "sUsername")
     private String username;
 
