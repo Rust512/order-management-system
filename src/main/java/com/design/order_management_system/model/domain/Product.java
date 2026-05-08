9b47825319cd9503dc2ebe2a4f27f6d4ac77c4b7
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 10:41:14 +0530

    Add the Product entity

---
+++ src/main/java/com/design/order_management_system/model/domain/Product.java
@@ -0,0 +1,39 @@
+package com.design.order_management_system.model.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
+    @Positive
+    private BigDecimal price;
+
+    @Min(value = 0L)
+    private Long stock;
+}
