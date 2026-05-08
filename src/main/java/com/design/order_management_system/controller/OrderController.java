52173b0ce3bb6085cf9c50ead40c6c18490d064f
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-08 20:23:58 +0530

    Add a controller method to get an order by ID

--- src/main/java/com/design/order_management_system/controller/OrderController.java
+++ src/main/java/com/design/order_management_system/controller/OrderController.java
@@ -6,6 +6,8 @@ import com.design.order_management_system.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,10 @@ public class OrderController {
     @PostMapping
     ResponseEntity<OrderResponse> registerOrder(@RequestBody @Valid OrderRequest orderRequest) {
         return ResponseEntity.ok(orderService.registerOrder(orderRequest));
+    }
+
+    @GetMapping(path = "/{id}")
+    ResponseEntity<OrderResponse> getOrder(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.getOrderById(id));
     }
 }
