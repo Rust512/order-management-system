566872bcde4cd3b5a5f7e3680d1d49f7e1ebd23f
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-09 17:47:43 +0530

    Add security filter chain bean for security config

--- src/main/java/com/design/order_management_system/config/SecurityConfig.java
+++ src/main/java/com/design/order_management_system/config/SecurityConfig.java
@@ -1,12 +1,44 @@
 package com.design.order_management_system.config;
 
+import com.design.order_management_system.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        return httpSecurity.formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth/**")
+                            .permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "/v1/user")
+                            .hasRole("ADMIN");
+                    auth.anyRequest()
+                            .authenticated();
+                })
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
