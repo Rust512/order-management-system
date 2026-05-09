021c0b68413656b9bb9975df77a11960ffa93535
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-09 20:42:12 +0530

    Add user ID to the claims in the JWT token

--- src/main/java/com/design/order_management_system/utils/SecurityUtils.java
+++ src/main/java/com/design/order_management_system/utils/SecurityUtils.java
@@ -1,11 +1,11 @@
 package com.design.order_management_system.utils;
 
+import com.design.order_management_system.dto.security.PrincipalUser;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.impl.security.StandardSecureDigestAlgorithms;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.SecretKey;
 import java.time.Instant;
@@ -20,15 +20,22 @@ public class SecurityUtils {
     }
 
     public static final String ROLES = "roles";
+    public static final String USER_ID = "userId";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String SECURITY_CONTEXT_EMPTY = "Security context empty";
+    public static final String PRINCIPAL_IS_NULL = "Principal is null!";
     private static final long TOKEN_EXPIRY_IN_MINUTES = 10L;
     private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
 
-    public static String generateJwtToken(UserDetails user) {
+    public static String generateJwtToken(PrincipalUser user) {
         List<String> roles = user.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
                 .toList();
-        var claims = Map.of(ROLES, roles);
+        var claims = Map.of(
+                ROLES, roles,
+                USER_ID, user.getUserId()
+        );
         return Jwts.builder()
                 .claims(claims)
                 .subject(user.getUsername())
