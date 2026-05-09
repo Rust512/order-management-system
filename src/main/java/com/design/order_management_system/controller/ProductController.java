b222abd0bbe400aba041d1fca8ec664388537e50
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-09 22:07:29 +0530

    Add a controller to update product by ID

--- src/main/java/com/design/order_management_system/controller/ProductController.java
+++ src/main/java/com/design/order_management_system/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.design.order_management_system.controller;
 
 import com.design.order_management_system.dto.request.CreateProductRequest;
+import com.design.order_management_system.dto.request.ProductUpdateRequest;
 import com.design.order_management_system.dto.response.PagedResponse;
 import com.design.order_management_system.dto.response.ProductResponse;
 import com.design.order_management_system.service.ProductService;
@@ -9,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,5 +33,10 @@ public class ProductController {
     @GetMapping
     ResponseEntity<PagedResponse<ProductResponse>> getProducts(@RequestParam int page, @RequestParam int size) {
         return ResponseEntity.ok(productService.getProducts(page, size));
+    }
+
+    @PutMapping(path = "/{id}")
+    ResponseEntity<ProductResponse> updateProduct(@PathVariable long id, @RequestBody @Valid ProductUpdateRequest updateRequest) {
+        return ResponseEntity.ok(productService.updateProduct(id, updateRequest));
     }
 }
