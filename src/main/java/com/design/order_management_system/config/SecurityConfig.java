430c0941c696fbe3db34dfe4bf884a306f5f3595
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-09 19:32:53 +0530

    Authorize /v1/products for ADMIN role only

--- src/main/java/com/design/order_management_system/config/SecurityConfig.java
+++ src/main/java/com/design/order_management_system/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/auth/**")
                             .permitAll();
-                    auth.requestMatchers(HttpMethod.POST, "/v1/user")
+                    auth.requestMatchers(HttpMethod.POST, "/v1/user", "/v1/products")
                             .hasRole("ADMIN");
                     auth.anyRequest()
                             .authenticated();
