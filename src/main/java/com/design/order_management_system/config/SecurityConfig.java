f23c12331f2f7b9b2d52257535305d58c4439900
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-10 13:22:37 +0530

    Remove redundant imports

--- src/main/java/com/design/order_management_system/config/SecurityConfig.java
+++ src/main/java/com/design/order_management_system/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.design.order_management_system.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
