3986e31557f8909ada43e1bc990d5b3754093215
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-10 08:39:03 +0530

    Seed the USER and ADMIN roles

---
+++ src/main/java/com/design/order_management_system/config/seeder/RoleSeeder.java
@@ -0,0 +1,38 @@
+package com.design.order_management_system.config.seeder;
+
+import com.design.order_management_system.constants.CommonConstants;
+import com.design.order_management_system.model.security.Role;
+import com.design.order_management_system.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class RoleSeeder implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    public static final String ROLE_USER_WAS_NOT_SEEDED = "ROLE_USER was not seeded";
+
+    @Override
+    @NullMarked
+    public void run(String... args) {
+        var userRole = Role.builder()
+                .name(CommonConstants.ROLE_USER)
+                .build();
+        var adminRole = Role.builder()
+                .name(CommonConstants.ROLE_ADMIN)
+                .build();
+
+        var roleMap = Map.of(CommonConstants.ROLE_USER, userRole, CommonConstants.ROLE_ADMIN, adminRole);
+
+        roleMap.entrySet()
+                .stream()
+                .filter(entry -> roleRepository.existsByName(entry.getKey()))
+                .forEach(entry -> roleRepository.save(entry.getValue()));
+    }
+}
--- src/main/java/com/design/order_management_system/constants/CommonConstants.java
+++ src/main/java/com/design/order_management_system/constants/CommonConstants.java
@@ -12,6 +12,7 @@ public class CommonConstants {
     public static final String ORDER = "ORDER";
     public static final String PRODUCT = "PRODUCT";
     public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String BEARER_TOKEN_PREFIX = "Bearer ";
     public static final String AUTHORIZATION_HEADER_KEY = "Authorization";
 }
