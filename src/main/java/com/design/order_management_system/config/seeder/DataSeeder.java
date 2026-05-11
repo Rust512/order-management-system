30d8747b7c2173047da6111ecd71b01d6f184c6a
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-11 11:58:36 +0530

    Move the admin seeder to the role seeder an rename

--- src/main/java/com/design/order_management_system/config/seeder/DataSeeder.java
+++ src/main/java/com/design/order_management_system/config/seeder/DataSeeder.java
@@ -2,10 +2,13 @@ package com.design.order_management_system.config.seeder;
 
 import com.design.order_management_system.constants.CommonConstants;
 import com.design.order_management_system.model.security.Role;
+import com.design.order_management_system.model.security.User;
 import com.design.order_management_system.repository.RoleRepository;
+import com.design.order_management_system.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,11 +17,16 @@ import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
-public class RoleSeeder implements CommandLineRunner {
+public class DataSeeder implements CommandLineRunner {
 
+    private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public static final String ROLE_USER_WAS_NOT_SEEDED = "ROLE_USER was not seeded";
+
+    private static final String ADMIN_USERNAME = "ADMIN";
+    private static final String ADMIN_PASSWORD = "Admin@123";
 
     @Override
     @NullMarked
@@ -33,5 +41,17 @@ public class RoleSeeder implements CommandLineRunner {
                 .filter(entry -> !existingRoles.contains(entry))
                 .map(entry -> Role.builder().name(entry).build())
                 .forEach(roleRepository::save);
+
+        var user = User.builder()
+                .username(ADMIN_USERNAME)
+                .password(passwordEncoder.encode(ADMIN_PASSWORD))
+                .build();
+        var adminRole = roleRepository.findByName(CommonConstants.ROLE_ADMIN)
+                .orElseThrow(() -> new IllegalStateException(DataSeeder.ROLE_USER_WAS_NOT_SEEDED));
+        user.addRole(adminRole);
+        boolean exists = userRepository.existsByUsername(ADMIN_USERNAME);
+        if (!exists) {
+            userRepository.save(user);
+        }
     }
 }
