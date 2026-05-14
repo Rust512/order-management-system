c99acfc2d4275fbc535432bc9195e23398eb4b59
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-14 17:30:55 +0530

    Add sample request for login and user registration

---
+++ src/main/java/com/design/order_management_system/constants/swagger/SwaggerRequestExamples.java
@@ -0,0 +1,19 @@
+package com.design.order_management_system.constants.swagger;
+
+public class SwaggerRequestExamples {
+    private SwaggerRequestExamples() {
+    }
+
+    public static final String LOGIN = """
+            {
+                "sUsername": "ADMIN",
+                "sPassword": "Admin@123"
+            }
+            """;
+    public static final String USER_REGISTRATION = """
+            {
+                "sUsername": "username",
+                "sPassword": "Some$Password@432"
+            }
+            """;
+}
