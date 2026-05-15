844baa7e2cad52cee182576e575d4c4bdb78e673
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-15 09:52:36 +0530

    Add negative path test for the update product API

--- src/test/java/com/design/order_management_system/service/ProductServiceTest.java
+++ src/test/java/com/design/order_management_system/service/ProductServiceTest.java
@@ -1,10 +1,13 @@
 package com.design.order_management_system.service;
 
 import com.design.order_management_system.constants.CommonConstants;
+import com.design.order_management_system.constants.ErrorMessageConstants;
 import com.design.order_management_system.converter.CreateProductRequestToProduct;
 import com.design.order_management_system.converter.ProductToProductResponse;
 import com.design.order_management_system.dto.request.CreateProductRequest;
+import com.design.order_management_system.dto.request.ProductUpdateRequest;
 import com.design.order_management_system.exception.DuplicateResourceException;
+import com.design.order_management_system.model.domain.Product;
 import com.design.order_management_system.repository.ProductRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
@@ -43,7 +47,7 @@ class ProductServiceTest {
         String productName = "P0";
         var request = CreateProductRequest.builder()
                 .productName(productName)
-                .price(BigDecimal.valueOf(1.0))
+                .price(BigDecimal.valueOf(1))
                 .stock(1L)
                 .build();
 
@@ -57,10 +61,51 @@ class ProductServiceTest {
                         CommonConstants.NAME,
                         productName)
                 );
-        
+
         verify(productRepository).existsByName(productName);
         verify(productRepository, never()).save(any());
         verifyNoMoreInteractions(productRepository);
         verifyNoInteractions(createProductRequestToProduct, productToProductResponse);
+    }
+
+    @Test
+    @DisplayName(value = """
+            When the stockToAdd parameter in the update product service request body
+            is less than the negated current stock of the corresponding product;
+            i.e. updatedStock = stockToUpdate + currentStock < 0
+            the service should throw an IllegalArgumentException.
+            """)
+    void updateProduct_WhenStockToAddLessThanNegatedCurrentStock_ShouldThrowIllegalArgumentException() {
+        Long productId = 1L;
+        Long stock = 2L;
+        String productName = "P0";
+        BigDecimal price = BigDecimal.ONE;
+
+        var product = Product.builder()
+                .id(productId)
+                .name(productName)
+                .price(price)
+                .stock(stock)
+                .build();
+
+        // -stock - 1 is always less than -stock 
+        var updateProductRequest = ProductUpdateRequest.builder()
+                .newProductName(productName)
+                .updatedPrice(BigDecimal.ONE)
+                .stockToAdd(-stock - 1)
+                .build();
+
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        Assertions.assertThatThrownBy(() -> productService.updateProduct(productId, updateProductRequest))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessageConstants.PRODUCT_STOCK_CANNOT_BE_NEGATIVE);
+
+        Assertions.assertThat(product.getStock()).isEqualTo(stock);
+
+        verify(productRepository).findById(productId);
+        verify(productRepository, never()).save(any());
+        verifyNoMoreInteractions(productRepository);
+        verifyNoInteractions(productToProductResponse);
     }
 }
