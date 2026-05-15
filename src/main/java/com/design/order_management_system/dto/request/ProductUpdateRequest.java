4d4871438270dace90f76a895881fb934e7591db
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-15 09:52:15 +0530

    Add builders to the ProductUpdateRequest DTO.

--- src/main/java/com/design/order_management_system/dto/request/ProductUpdateRequest.java
+++ src/main/java/com/design/order_management_system/dto/request/ProductUpdateRequest.java
@@ -2,11 +2,15 @@ package com.design.order_management_system.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class ProductUpdateRequest {
     @JsonProperty(value = "sNewProductName")
     private String newProductName;
