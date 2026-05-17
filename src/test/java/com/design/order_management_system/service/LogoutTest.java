6d6900d8d19fc8c76beef64a471d8fe96c9b2a9b
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-17 20:50:15 +0530

    Add logout happy path test

--- src/test/java/com/design/order_management_system/service/LogoutTest.java
+++ src/test/java/com/design/order_management_system/service/LogoutTest.java
@@ -1,19 +1,24 @@
 package com.design.order_management_system.service;
 
 import com.design.order_management_system.constants.CommonConstants;
+import com.design.order_management_system.model.security.RevokedToken;
 import com.design.order_management_system.repository.RevokedTokenRepository;
 import com.design.order_management_system.utils.HashUtils;
 import com.design.order_management_system.utils.SecurityUtils;
 import com.design.order_management_system.utils.TestSecurityUtils;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -45,10 +50,10 @@ class LogoutTest {
 
     @Test
     @DisplayName(value = """
-            POST /auth/logout without a revoked auth token
+            POST /auth/logout with a revoked auth token
             should logout, but no revoked token entry should be present in the DB.
             """)
-    void logout_WithRevokedToken_ShouldReturnStatus401() throws Exception {
+    void logout_WithRevokedToken_ShouldNotSavedRevokedTokenEntity() throws Exception {
         var user = SecurityUtils.getPrincipalUser();
         var token = SecurityUtils.generateJwtToken(user);
         var hash = HashUtils.sha256(token);
@@ -61,6 +66,40 @@ class LogoutTest {
 
         verify(revokedTokenRepository).existsByTokenHashAndExpiresAtAfter(eq(hash), any());
         verify(revokedTokenRepository).existsByTokenHash(hash);
+        verifyNoMoreInteractions(revokedTokenRepository);
+    }
+
+    @Test
+    @DisplayName(value = """
+            POST /auth/logout with a valid auth token
+            should logout, and a revoked token entry should be saved in the DB.
+            """)
+    void logout_WithValidToken_ShouldReturnStatus401() throws Exception {
+        var user = SecurityUtils.getPrincipalUser();
+        var token = SecurityUtils.generateJwtToken(user);
+        var hash = HashUtils.sha256(token);
+        var revokedToken = RevokedToken.builder()
+                .id(1L)
+                .tokenHash(hash)
+                .expiresAt(user.getExpiryTime())
+                .revokedAt(Instant.now())
+                .build();
+
+        when(revokedTokenRepository.existsByTokenHashAndExpiresAtAfter(eq(hash), any())).thenReturn(false);
+        when(revokedTokenRepository.existsByTokenHash(hash)).thenReturn(false);
+        when(revokedTokenRepository.save(any())).thenReturn(revokedToken);
+        mockMvc.perform(post("/auth/logout")
+                        .header(CommonConstants.AUTHORIZATION_HEADER_KEY, CommonConstants.BEARER_TOKEN_PREFIX + token))
+                .andExpect(status().isOk());
+
+        verify(revokedTokenRepository).existsByTokenHashAndExpiresAtAfter(eq(hash), any());
+        verify(revokedTokenRepository).existsByTokenHash(hash);
+
+        ArgumentCaptor<RevokedToken> revokedTokenCaptor = ArgumentCaptor.forClass(RevokedToken.class);
+        verify(revokedTokenRepository).save(revokedTokenCaptor.capture());
+        var unsavedRevokedToken = revokedTokenCaptor.getValue();
+        Assertions.assertThat(unsavedRevokedToken.getTokenHash()).isEqualTo(hash);
+
         verifyNoMoreInteractions(revokedTokenRepository);
     }
 }
