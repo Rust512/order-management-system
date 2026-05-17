6941f40ab0d4e20f89df430d995afcfc60e794e4
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-17 12:31:11 +0530

    All AllArgsConstructor to PrincipalUser

--- src/main/java/com/design/order_management_system/model/security/PrincipalUser.java
+++ src/main/java/com/design/order_management_system/model/security/PrincipalUser.java
@@ -1,5 +1,6 @@
 package com.design.order_management_system.model.security;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class PrincipalUser implements UserDetails {
 
     private final Long userId;
