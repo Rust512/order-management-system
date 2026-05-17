00744ad645f036ee0d8a4c81e70d788cba93a097
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-17 15:27:31 +0530

    Add custom annotation to exclude custom filters
    The code changes in this commit add a new custom annotation
    @WebMvcSliceTest to exclude the LogoutFilter (due to custom
    LogoutHandler implementation) and the JwtAuthenticationFilter. These
    filters have a repository bean dependency which is not loaded by the
    @WebMvcTest annotation.

---
+++ src/test/java/com/design/order_management_system/annotation/WebMvcSliceTest.java
@@ -0,0 +1,37 @@
+package com.design.order_management_system.annotation;
+
+import com.design.order_management_system.security.JwtAuthenticationFilter;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Use this annotation in a WebMvcTest where you do not want
+ * to test the login and logout procedures
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@WebMvcTest(
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = {
+                        JwtAuthenticationFilter.class,
+                        LogoutFilter.class
+                }
+        )
+)
+public @interface WebMvcSliceTest {
+
+    @AliasFor(
+            annotation = WebMvcTest.class,
+            attribute = "controllers"
+    )
+    Class<?>[] value() default {};
+}
