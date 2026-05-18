a29cb57b5fa9a5957360e7af693a6a2aec502d1e
Devang Hemant Bhagwat <devanghb@lentra.ai>
2026-05-18 08:15:26 +0530

    Add repository methods to fetch audit entries

--- src/main/java/com/design/order_management_system/repository/ProductAuditEntryRepository.java
+++ src/main/java/com/design/order_management_system/repository/ProductAuditEntryRepository.java
@@ -1,8 +1,13 @@
 package com.design.order_management_system.repository;
 
 import com.design.order_management_system.model.domain.ProductAuditEntry;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProductAuditEntryRepository extends JpaRepository<ProductAuditEntry, Long> {
     @Query(value = """
@@ -11,4 +16,10 @@ public interface ProductAuditEntryRepository extends JpaRepository<ProductAuditE
             WHERE pae.product.id = :productId
             """)
     Long getNextAuditVersionByProductId(Long productId);
+
+    @EntityGraph(attributePaths = "user")
+    Page<ProductAuditEntry> findByProduct_Id(Long productId, Pageable pageable);
+
+    @EntityGraph(attributePaths = "user")
+    Optional<ProductAuditEntry> findByProduct_IdAndVersion(Long productId, Long version);
 }
