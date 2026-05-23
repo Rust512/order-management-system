860beda7342b1cf1ca580c9435f1a70508613f4e
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-23 22:45:50 +0530

    Add new column reservedStock to the product schema

--- src/main/java/com/design/order_management_system/model/domain/Product.java
+++ src/main/java/com/design/order_management_system/model/domain/Product.java
@@ -41,4 +41,10 @@ public class Product {
     @NotNull
     @PositiveOrZero
     private Long stock;
+    
+    @NotNull
+    @PositiveOrZero
+    @Builder.Default
+    @Column(nullable = false)
+    private Long reservedStock = 0L;
 }
