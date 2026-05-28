f2352f75be3d6ec2fbe50f9a0382e8898bba8ee3
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-05-28 07:33:56 +0530

    Remove redundant @Modifying from find query

--- src/main/java/com/design/order_management_system/repository/ProductRepository.java
+++ src/main/java/com/design/order_management_system/repository/ProductRepository.java
@@ -4,7 +4,6 @@ import com.design.order_management_system.model.domain.Product;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -19,7 +18,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             """)
     boolean existsByName(@Param("name") String name);
 
-    @Modifying
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query(value = """
             SELECT product
