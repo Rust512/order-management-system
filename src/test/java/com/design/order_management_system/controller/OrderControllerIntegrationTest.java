faeb1069f06bdcd2924fc07d78f2e3ebf9948b52
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-06-01 20:31:24 +0530

    Implement happy-path tests for cancel order API
    This commit implements two happy-path tests for the DELETE /v1/orders
    API.
    1. When draft order exists, but has no items in it.
    2. When draft order exists, with only one item in it.

--- src/test/java/com/design/order_management_system/controller/OrderControllerIntegrationTest.java
+++ src/test/java/com/design/order_management_system/controller/OrderControllerIntegrationTest.java
@@ -27,6 +27,7 @@ import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRe
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -135,11 +136,125 @@ public class OrderControllerIntegrationTest extends DatabaseTest {
                     .orElseThrow();
             Assertions.assertThat(persistedProduct.getStock()).isEqualTo(stock - quantity);
             Assertions.assertThat(persistedProduct.getReservedStock()).isEqualTo(reservedStock - quantity);
-            
             var persistedOrder = orderRepository.findById(savedOrder.getId())
                     .orElseThrow();
-            
+
             Assertions.assertThat(persistedOrder.getOrderStatus()).isEqualTo(OrderStatus.CONFIRMED);
+        });
+    }
+
+    @Test
+    @DisplayName(value = """
+            If the draft order corresponding to the logged-in user exists with NO order item,
+            the DELETE /v1/orders API should return an OrderResponse with order status CANCELLED.
+            """)
+    void cancelOrder_WhenDraftOrderHasNoItems_ThenShouldChangeOrderStatusAndUpdateProductStock() {
+        var owner = userRepository.findByUsername(NORMAL_USERNAME).orElseThrow();
+
+        var savedOrder = transactionTemplate.execute(_ -> {
+            var order = Order.builder()
+                    .orderStatus(OrderStatus.CREATED)
+                    .user(owner)
+                    .build();
+            return orderRepository.save(order);
+        });
+
+        var headers = new HttpHeaders();
+        setAuthorizationHeader(headers);
+
+        var entity = new HttpEntity<>(headers);
+
+        ResponseEntity<OrderResponse> response = this.restTemplate.exchange("/v1/orders", HttpMethod.DELETE, entity, OrderResponse.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+
+        var body = response.getBody();
+        Assertions.assertThat(body.getOrderId()).isEqualTo(savedOrder.getId());
+        Assertions.assertThat(body.getOrderStatus()).isEqualTo(OrderStatus.CANCELLED);
+        Assertions.assertThat(body.getCreatedAt()).isNotNull();
+        Assertions.assertThat(body.getTotalPrice()).isZero();
+        Assertions.assertThat(body.getOrderItems()).isEmpty();
+
+        transactionTemplate.executeWithoutResult(_ -> {
+            var persistedOrder = orderRepository.findById(savedOrder.getId())
+                    .orElseThrow();
+
+            Assertions.assertThat(persistedOrder.getOrderStatus()).isEqualTo(OrderStatus.CANCELLED);
+        });
+    }
+
+    @Test
+    @DisplayName(value = """
+            If the draft order corresponding to the logged-in user exists with one order item,
+            the DELETE /v1/orders API should return an OrderResponse with order status CANCELLED;
+            the product stock corresponding to the order items in the order should be updated.
+            """)
+    void cancelOrder_WhenDraftOrderExists_ThenShouldChangeOrderStatusAndUpdateProductStock() {
+        var owner = userRepository.findByUsername(NORMAL_USERNAME).orElseThrow();
+        var productName = GeneratorUtils.generateUUID();
+        var productPrice = BigDecimal.TWO;
+        var stock = 5L;
+        var reservedStock = 3L;
+        var savedProduct = transactionTemplate.execute(_ -> {
+            var product = Product.builder()
+                    .name(productName)
+                    .price(productPrice)
+                    .stock(stock)
+                    .reservedStock(reservedStock)
+                    .build();
+            return productRepository.save(product);
+        });
+
+        var quantity = 2L;
+        var savedOrder = transactionTemplate.execute(_ -> {
+            var orderItem = OrderItem.builder()
+                    .product(savedProduct)
+                    .quantity(quantity)
+                    .purchasePrice(productPrice)
+                    .build();
+            var order = Order.builder()
+                    .orderStatus(OrderStatus.CREATED)
+                    .user(owner)
+                    .build();
+            order.addOrderItem(orderItem);
+            return orderRepository.save(order);
+        });
+
+        var headers = new HttpHeaders();
+        setAuthorizationHeader(headers);
+
+        var entity = new HttpEntity<>(headers);
+
+        ResponseEntity<OrderResponse> response = this.restTemplate.exchange("/v1/orders", HttpMethod.DELETE, entity, OrderResponse.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody()).isNotNull();
+
+        var body = response.getBody();
+        Assertions.assertThat(body.getOrderId()).isEqualTo(savedOrder.getId());
+        Assertions.assertThat(body.getOrderStatus()).isEqualTo(OrderStatus.CANCELLED);
+        Assertions.assertThat(body.getCreatedAt()).isNotNull();
+        var expectedTotalPrice = BigDecimal.valueOf(quantity).multiply(productPrice);
+        Assertions.assertThat(body.getTotalPrice()).isEqualByComparingTo(expectedTotalPrice);
+        Assertions.assertThat(body.getOrderItems())
+                .singleElement()
+                .satisfies(orderItem -> {
+                    Assertions.assertThat(orderItem.getProductName()).isEqualTo(productName);
+                    Assertions.assertThat(orderItem.getQuantity()).isEqualTo(quantity);
+                    Assertions.assertThat(orderItem.getPurchasePrice()).isEqualByComparingTo(productPrice);
+                });
+
+        transactionTemplate.executeWithoutResult(_ -> {
+            var persistedProduct = productRepository.findById(savedProduct.getId())
+                    .orElseThrow();
+            Assertions.assertThat(persistedProduct.getStock()).isEqualTo(stock);
+            Assertions.assertThat(persistedProduct.getReservedStock()).isEqualTo(reservedStock - quantity);
+
+            var persistedOrder = orderRepository.findById(savedOrder.getId())
+                    .orElseThrow();
+
+            Assertions.assertThat(persistedOrder.getOrderStatus()).isEqualTo(OrderStatus.CANCELLED);
         });
     }
 
