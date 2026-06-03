3452e6782e4004133dcfce9d2c6d001855ffea91
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2026-06-03 16:54:21 +0530

    Add negative tests for SecurityUtils

---
+++ src/test/java/com/design/order_management_system/utils/SecurityUtilsTest.java
@@ -0,0 +1,67 @@
+package com.design.order_management_system.utils;
+
+import com.design.order_management_system.model.security.PrincipalUser;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.access.AuthorizationServiceException;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SecurityUtilsTest {
+
+    @Test
+    @DisplayName(value = """
+            If the list of roles in the given principal user object is empty,
+            the isAdmin method should return false.
+            """)
+    void isAdmin() {
+        var user = mock(PrincipalUser.class);
+        when(user.getRoles()).thenReturn(List.of());
+        Assertions.assertThat(SecurityUtils.isAdmin(user)).isFalse();
+    }
+
+    @Test
+    @DisplayName("""
+            If the provided authentication object is null,
+            the getPrincipalUserFromAuthentication method should throw AuthorizationServiceException
+            """)
+    void getPrincipalUserFromAuthentication_WhenAuthenticationNull_ShouldThrowAuthorizationServiceException() {
+        Assertions.assertThatThrownBy(() -> SecurityUtils.getPrincipalUserFromAuthentication(null))
+                .isInstanceOf(AuthorizationServiceException.class)
+                .hasMessage("Security context empty");
+    }
+
+    @Test
+    @DisplayName("""
+            If the principal in the provided authentication object is null,
+            the getPrincipalUserFromAuthentication method should throw AuthorizationServiceException
+            """)
+    void getPrincipalUserFromAuthentication_WhenAuthenticationPrincipalNull_ShouldThrowAuthorizationServiceException() {
+        var authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(null);
+        Assertions.assertThatThrownBy(() -> SecurityUtils.getPrincipalUserFromAuthentication(authentication))
+                .isInstanceOf(AuthorizationServiceException.class)
+                .hasMessage("Missing principal user");
+    }
+
+    @Test
+    @DisplayName("""
+            If the principal in the provided authentication object is not an instance of the PrincipalUser class,
+            the getPrincipalUserFromAuthentication method should throw AuthorizationServiceException
+            """)
+    void getPrincipalUserFromAuthentication_WhenAuthenticationPrincipalNotPrincipalUserInstance_ShouldThrowAuthorizationServiceException() {
+        var authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(new Object());
+        Assertions.assertThatThrownBy(() -> SecurityUtils.getPrincipalUserFromAuthentication(authentication))
+                .isInstanceOf(AuthorizationServiceException.class)
+                .hasMessage("Invalid Authentication Principal");
+    }
+}
